7fb41d448b2cf2b324d26d4f4cfb98dc73853f89
neurino0717 <liuenmao@hotmail.com>
2019-08-18 20:27:23 +0800

    all

--- examples/time_yearmonthday.cpp
+++ examples/time_yearmonthday.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main (){
-time_t now = time(0);
-tm tm = *localtime(&now);
-int year = tm.tm_year + 1900;
-int month = tm.tm_mon + 1;
-int day = tm.tm_mday;
-int hour = tm.tm_hour;
-int minute = tm.tm_min;
-int second = tm.tm_sec;
-int dayOfWeek = tm.tm_wday;
-cout << year << ":" << month  << ":" << day << ":" << hour << ":" << minute << ":" << second << endl;
+	time_t now = time(0);
+	tm tm = *localtime(&now);
+	int year = tm.tm_year + 1900;
+	int month = tm.tm_mon + 1;
+	int day = tm.tm_mday;
+	int hour = tm.tm_hour;
+	int minute = tm.tm_min;
+	int second = tm.tm_sec;
+	int dayOfWeek = tm.tm_wday;
+	cout << year << ":" << month  << ":" << day << ":" << hour << ":" << minute << ":" << second << endl;
 
-tm={2, 30, 22, 17, 7-1, 1975-1900};
-year = tm.tm_year + 1900;
-month = tm.tm_mon + 1;
-day = tm.tm_mday;
-hour = tm.tm_hour;
-minute = tm.tm_min;
-second = tm.tm_sec;
-dayOfWeek = tm.tm_wday;
-cout << year << ":" << month  << ":" << day << ":" << hour << ":" << minute << ":" << second << endl;
-    
+	tm={2, 30, 22, 17, 7-1, 1975-1900};
+	year = tm.tm_year + 1900;
+	month = tm.tm_mon + 1;
+	day = tm.tm_mday;
+	hour = tm.tm_hour;
+	minute = tm.tm_min;
+	second = tm.tm_sec;
+	dayOfWeek = tm.tm_wday;
+	cout << year << ":" << month  << ":" << day << ":" << hour << ":" << minute << ":" << second << endl;
+
 }
--- review.cpp
+++ review.cpp
@@ -200,3 +200,35 @@ Season winter = Winter;
 int baseValue = winter;           //2
 std::cout <<  Fall << std::endl;  //1
 std::cout << Fall2 << std::endl;  //1
+
+///////////constructions /////////////////////////
+class Dog{};
+/* C++ 03:
+ * 1. default constructor      (generated only if no constructor is declared by user)
+ * 2. copy constructor         (generated only if no (5|6) declared by user)
+ * 3. copy assignment operator (generated only if no (5|6) declared by user)
+ * 4. destructor
+ *
+ * c++ 11:
+ * 5. move constructor         (generated only if no (2|3|4|6) declared by user)
+ * 6. move assinment operator  (generated only if no (2|3|5|6) declared by user)
+ */
+class Cat{ //3, 4
+  Cat(const Cat&){} //copy constructor
+};
+class Duck{ //4
+  Duck(Duck&&){}    //move constructor
+};
+class Frog{ //4
+  Frog(Frog&&, int = 0) {} //move constructor
+  Frog(int = 0){}          //default constructor
+  Frog(const Frog&,int=0) {} //copy constructor
+};
+class Fish{ //1, 2, 3
+  ~Fish(){}
+};
+class Cow{ //1, 2, 4
+  Cow& operator=(const Cow&) = delete;
+};
+int main(){}
+
